da41d95be2604eabe20f3df1f3c99ab583c28b96
Shohan111369 <sarkershohan346@gmail.com>
2026-06-18 18:57:40 +0800

    connect with mongodb and run

--- index.js
+++ index.js
@@ -1,19 +1,54 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const cors = require('cors');
-const jwt = require('jsonwebtoken'); 
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 dotenv.config();
-
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 
-   
+app.use(cors());
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('AI Prompt Sharing Platform Server Running...');
+const uri = process.env.MONGODB_URI;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    const db = client.db("AiPromptDB");
+    const organizationCollection = db.collection("organizations");
+    const eventsCollection = db.collection("events");
+    const usersCollection = db.collection("user");
+    const bookingCollection = db.collection("bookings");
+    const paymentCollection = db.collection("payments");
+
+    app.get("/api/organization/:email", async (req, res) => {
+      const { email } = req.params;
+      const result = await organizationCollection.findOne({
+        organizerEmail: email,
+      });
+      res.send(result);
+    });
+
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+  console.log(`Example app listening on port ${port}`);
 });
