152279cce0389d015fa69562ef42c21cf97a16e9
Shohan111369 <sarkershohan346@gmail.com>
2026-06-26 19:34:27 +0800

    error solve

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: ["https://ai-prompt-client-amber.vercel.app"], // আপনার ফ্রন্টএন্ডের URL
+  credentials: true,
+}));
 app.use(express.json());
 
 const uri = process.env.MONGODB_URI;
